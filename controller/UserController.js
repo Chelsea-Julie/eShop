a4a64246041b615e166ad65003773846d5346740
Chelsea-Julie <chelseaqjulie.gmail.com>
2024-08-13 14:11:49 +0200

    controllers

---
+++ controller/UserController.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import path from 'path'
+import bodyParser from 'body-parser'
+import { user } from '../model/index'
+
+const userRouter = express.Router()
+
+userRouter.use(bodyParser.json())
+userRouter.get('/', (req, res) => {
+    user.fetchUsers(req, res)
+})
+
+userRouter.get('/:id', (req, res) => {
+    user.fetchOneUser(req, res)
+})
+
+userRouter.patch('/user/:id', (req, res) => {
+    user.updated(req, res)
+})
+
+userRouter.delete('/user/:id', (req, res) => {
+    user.deleteUser(req, res)
+})
+
+userRouter.post('/register', (req, res) => {
+    user.registerUser(req, res)
+})
+
+userRouter.post('/login', (req, res) => {
+    user.loginUser(req, res)
+})
