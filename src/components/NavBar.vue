3385d1e85eb26fe11812bdeaf83ae457b952a766
Chelsea-Julie <chelseaqjulie.gmail.com>
2024-08-17 09:07:04 +0200

    any

---
+++ src/components/NavBar.vue
@@ -0,0 +1,37 @@
+<template>
+    <nav class="navbar navbar-expand-lg sticky-top">
+  <div class="container-fluid">
+    <router-Link class="navbar-brand" to="/">
+        <img src="https://chelsea-julie.github.io/all-images/images/Random/cj.png" alt="logo" class="img-thumbnail" loading="lazy">
+  </router-link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active text-dark" aria-current="page" to="/">Home<i class="bi bi-house"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-dark" href="#about">About <i class="bi bi-file-earmark-person"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-dark" href="#resume">Resume <i class="bi bi-file-person"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-dark" href="#skills">Skills<i class="bi bi-lightbulb"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-dark" href="#project">Projects<i class="bi bi-card-list"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-dark" href="#testimonials">Testimonials<i class="bi bi-person-lines-fill"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-dark" href="#reachme">Reach me<i class="bi bi-telephone"></i></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
